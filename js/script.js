1ea91ce59a0b1c023bc46f4e6859898e7960eda4
kolob2000 <49161276+kolob2000@users.noreply.github.com>
2021-12-14 21:21:49 +0300

    edits

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 "use strict"
-const cardItem = document.querySelectorAll('.cart_link')
+const productCount = document.querySelector('.count_product');
+const cardItem = document.querySelectorAll('.cart_link');
+
 for (const i of cardItem) {
-    i.addEventListener('click', productToCart)
+    i.addEventListener('click', productToCart);
 
 }
+
 let count = 0;
 
 function productToCart(event) {
     event.preventDefault();
-
-
-    count += 1;
-    if (event.target.localName === 'a'){
-        if (event.target.innerText === 'Add to Cart') {
-        event.target.textContent = 'Product in cart';
-        // event.target.setAttribute('disabled', 'disabled');
-    }
+    // console.log(this.innerText);
+    if (this.innerText === 'Add to Cart') {
+        this.innerHTML = '<span class="cart_text" style="color: #a4a3a3;">Product in cart</span>';
+        this.style = 'pointer-events: none; cursor: default; border: #a4a3a3 1px solid;'
+        count += 1;
+        productCount.textContent = count;
+        productCount.style = 'display: inline;'
     }
 }
 
