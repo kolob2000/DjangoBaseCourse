7cc2bdd3089dcaebc68a85ff4c64a17f22b78fdc
Nick <k.kolabis@mail.ru>
2021-12-24 20:20:53 +0300

    edits

--- file.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const cross = document.querySelector(".menu-icons__cross");
-const cartBlock = document.querySelector('.popup-cart');
-cross.addEventListener('click', (event) => {
-    cartBlock.style = 'display:none;';
-    cross.style = 'display:none';
-});
-
